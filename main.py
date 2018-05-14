b71e23394b8bb2d2bd667104fcd0c74838df575f
Po Chun, Lu <oceanus11034@gmail.com>
2018-05-14 23:25:48 +0800

    [Add] Read Corpus

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from src.dataGenerator import (getEncodeData, splitData)
+import csv
+import pandas as pd
+
+from src.dataGenerator import (getData, encodeData, splitData)
 from src.model import buildModel
 from src.training import train
 from src.test import test
 
 CHARS = '0123456789- '
 DIGITS = 3
-TOTAL_SIZE = 80000
+TOTAL_SIZE = 100000
 BATCH_SIZE = 128
 
 DATA_SIZE = {
@@ -13,10 +16,39 @@ DATA_SIZE = {
   'TESTING_SIZE': 50000,
 }
 
-encodeData = getEncodeData(TOTAL_SIZE, DIGITS, CHARS)
+# Build Corpus Data
+# data = getData(TOTAL_SIZE, DIGITS)
+# # Save X Corpus Data
+# with open('./corpus/total_x.csv', 'w') as corpus_x:
+#     for question in data[0]:
+#         corpus_x.write(question + '\n')
+# corpus_x.close()
 
-test_training_size = [10000]
+# Save Y Corpus Data
+# with open('./corpus/total_y.csv', 'w') as corpus_y:
+#     for expect in data[1]:
+#         corpus_y.write(expect + '\n')
+# corpus_y.close()
 
+# Read Corpus
+data_x = list()
+with open('./corpus/total_x.csv', 'r') as x:
+    source = csv.reader(x, delimiter=',')
+    for row in source:
+        data_x.append(row[0])
+
+data_y = list()
+with open('./corpus/total_y.csv', 'r') as y:
+    source = csv.reader(y, delimiter=',')
+    for row in source:
+        data_y.append(row[0])
+
+data = [data_x, data_y]
+encodeData = encodeData(DIGITS, data, CHARS)
+
+test_training_size = [20000]
+
+# Iterate Different Training Size
 with open('./log/test_acc.csv', 'w') as output:
     output.write('model,test_acc\n')
     for training_size in test_training_size:
